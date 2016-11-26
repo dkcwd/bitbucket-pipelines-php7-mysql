5b73cbdf0f83151ebae40b6e30940c457755c4fe
Dave Clark <dave@dkcwd.com.au>
2016-11-27 10:59:09 +1100

    Update install-phantomjs.sh

--- install-phantomjs.sh
+++ install-phantomjs.sh
@@ -5,11 +5,10 @@ apt-get install build-essential chrpath libssl-dev libxft-dev -y
 apt-get install libfreetype6 libfreetype6-dev -y
 apt-get install libfontconfig1 libfontconfig1-dev -y
 #installation
-cd ~
+cd /usr/local/share
 export PHANTOM_JS="phantomjs-2.1.1-linux-x86_64"
 wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
 tar xvjf $PHANTOM_JS.tar.bz2
-mv $PHANTOM_JS.tar.bz2 /usr/local/share/
-ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/share/phantomjs
-ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin/phantomjs
-ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/bin/phantomjs
+ln -s /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/share/phantomjs
+ln -s /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin/phantomjs
+ln -s /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/bin/phantomjs
