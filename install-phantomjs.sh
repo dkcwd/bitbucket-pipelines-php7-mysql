7082d1ab7ceef5312d1a019a0d3955fa54c18d69
Dave Clark <dave@dkcwd.com.au>
2016-12-12 20:37:48 +1100

    Experimenting with rebuilt binary
    See: https://github.com/ariya/phantomjs/issues/11526
    - Was experiencing issues with running acceptance tests with PhantomJS on Bitbucket Pipelines
    - In particular see https://github.com/ariya/phantomjs/issues/11526#issuecomment-200959478

--- install-phantomjs.sh
+++ install-phantomjs.sh
@@ -6,9 +6,8 @@ apt-get install libfreetype6 libfreetype6-dev -y
 apt-get install libfontconfig1 libfontconfig1-dev -y
 #installation
 cd /usr/local/share
-export PHANTOM_JS="phantomjs-2.1.1-linux-x86_64"
-wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
-tar xvjf $PHANTOM_JS.tar.bz2
-ln -s /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/share/phantomjs
-ln -s /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin/phantomjs
-ln -s /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/bin/phantomjs
+wget https://s3-ap-southeast-2.amazonaws.com/phantomjs-dkcwd-workaround/phantomjs-dkcwd-2.1.1.tar.gz
+tar xvjf phantomjs-dkcwd-2.1.1.tar.gz
+ln -s /usr/local/share/phantomjs/bin/phantomjs /usr/local/share/phantomjs
+ln -s /usr/local/share/phantomjs/bin/phantomjs /usr/local/bin/phantomjs
+ln -s /usr/local/share/phantomjs/bin/phantomjs /usr/bin/phantomjs
